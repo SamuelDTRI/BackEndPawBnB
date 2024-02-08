6d6a57fa69b238f63afa115cacf25eee68ef9df2
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-02-08 19:58:21 -0300

    agregado comentario en el archivo mainRouter

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -7,7 +7,7 @@ const mainRouter = Router();
 mainRouter.get("/", (req, res) => {
   res.status(200).json("ok");
 });
-
+// ruta para el manejo del modelo Locations
 mainRouter.get("/locations", locationsHandler);
 
 module.exports = { mainRouter };
