43e7370f20ebe3d586514e62de333ec5bbe71c61
Nicolas Soto <nicosoto18@hotmail.com>
2024-02-12 20:32:11 -0300

    nada

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { server } = require("./src/server");
 
 console.log(
   "Is sequelize an instance of Sequelize?",
-  sequelize instanceof require("sequelize")
-);
-
+   sequelize instanceof require("sequelize")
+   );
+ 
 sequelize.sync({ alter: true }).then(
   server.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${process.env.PORT || 3000}`);
