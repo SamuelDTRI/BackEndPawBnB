f77c7988cdf1f9159992c8d26b2de1815b027432
sariditri31@gmail.com <sariditri31@gmail.com>
2024-02-23 09:57:23 -0500

    okeyyy

--- src/routes/Pay/payRouter.js
+++ src/routes/Pay/payRouter.js
@@ -3,8 +3,8 @@ const { createSession } = require("../../controllers/Pay/payController");
 
 const payRouter = Router();
 
-payRouter.post("/create-checkout-session/:id", createSession);
-payRouter.get("/success/:id", (req, res) => res.send("success"));
-payRouter.get("/cancel/:id", (req, res) => res.send("cancel"));
+payRouter.post("/create-checkout-session/", createSession);
+payRouter.get("/success/", (req, res) => res.send("success"));
+payRouter.get("/cancel/", (req, res) => res.send("cancel"));
 
 module.exports = { payRouter };
