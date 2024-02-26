96da3cdf35aec76d0bba7454b2a2d822059332d2
Nicolas Soto <nicosoto18@hotmail.com>
2024-02-26 04:39:18 -0300

    backend arreglado para sitters

--- src/controllers/Bookings/getBookingsByIdController.js
+++ src/controllers/Bookings/getBookingsByIdController.js
@@ -1,7 +1,15 @@
-const { Bookings } = require("../../db");
+const { Bookings,Dogs,Owners } = require("../../db");
 
 const getBookingsByIdController = async (id) => {
-  const bookingId = await Bookings.findByPk(id);
+  const bookingId = await Bookings.findByPk(id, {
+    include: [{
+    model: Dogs,    
+    },
+    {
+      model: Owners,
+    }
+  ]
+});
   return bookingId;
 };
 module.exports = { getBookingsByIdController };
